772e8f4666cff56fdbc6c7b4046992193b86e77a
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-09-09 07:30:01 +0100

    image fix

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,16 +1,15 @@
-import React from "react"
+import React from "react";
 
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
-
 
 function TeacherItem() {
 	return (
 		<article className="teacher-item">
 			<header>
 				<img
-					src="https://media-exp1.licdn.com/dms/image/C4E35AQFD7riu4O_Tmg/profile-framedphoto-shrink_200_200/0?e=1598598000&v=beta&t=GjFElLNYRJTLs_Z4wqo-65dJ_DQhZbcf3QQNhKYxpK0"
+					src="https://media-exp1.licdn.com/dms/image/C4E35AQFD7riu4O_Tmg/profile-framedphoto-shrink_200_200/0?e=1599721200&v=beta&t=ATIxJCDhcR5JHbsZPOY0igeWVVu0V5_Ej1Lg4svZsBE"
 					alt="Rui Pereira"
 				/>
 				<div>
@@ -20,23 +19,22 @@ function TeacherItem() {
 			</header>
 			<p>
 				Entusiasta das melhores tecnoligias de Programação.
-						<br />
 				<br />
-						Apaixonado por mudar pelo desenvolvimento de software.
-					</p>
+				<br />
+				Apaixonado por mudar pelo desenvolvimento de software.
+			</p>
 			<footer>
 				<p>
 					Preço/hora
-							<strong>15€</strong>
+					<strong>15€</strong>
 				</p>
 				<button type="button">
 					<img src={whatsappIcon} alt="whatsapp" />
-							Entrar em contacto
-						</button>
+					Entrar em contacto
+				</button>
 			</footer>
 		</article>
-	)
+	);
 }
-
 
 export default TeacherItem;
